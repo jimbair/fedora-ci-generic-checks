ded034ccbb803a2d99de8387226475c97bc76a28
Tim Flink <tflink@fedoraproject.org>
2020-01-15 11:11:51 -0700

    trying to move script approvals to init.groovy

--- config/s2i/jenkins/master/configuration/init.groovy
+++ config/s2i/jenkins/master/configuration/init.groovy
@@ -42,3 +42,21 @@ GlobalCIConfiguration.get().addMessageProvider(fedmsg)
 logger.info("Setup fedora-fedmsg-stage Messaging Provider")
 FedMsgMessagingProvider fedmsgStage = new FedMsgMessagingProvider("fedora-fedmsg-stage", "tcp://stg.fedoraproject.org:9940", "tcp://172.19.4.36:9941", "org.fedoraproject");
 GlobalCIConfiguration.get().addMessageProvider(fedmsgStage)
+
+// attempting to set script approvals
+def approval_signatures = [
+    "method java.lang.Class getSuperclass",
+    "method java.lang.Class isAssignableFrom java.lang.Class",
+    "method net.sf.json.JSONObject has java.lang.String",
+    "staticMethod java.lang.Class forName java.lang.String",
+    "staticMethod java.time.Instant now",
+    "staticMethod org.codehaus.groovy.runtime.StackTraceUtils printSanitizedStackTrace java.lang.Throwable",
+    "java.io.PrintWriter",
+    "staticMethod org.codehaus.groovy.runtime.DefaultGroovyMethods getAt java.util.List java.util.Collection",
+    "new java.lang.Exception java.lang.String java.lang.Throwable",
+    "new java.lang.Exception java.lang.String java.lang.Throwable"]
+
+def approval = org.jenkinsci.plugins.scriptsecurity.scripts.ScriptApproval.get()
+approval_signatures.each {signature -> approval.approveSignature(signature) }
+
+
